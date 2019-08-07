9f2abad91e8b0f2db7fe20e8048a8a663a9a7e6c
Dennis Brotzky <dennis@narative.co>
2019-08-07 11:02:41 -0700

    Updating to new Headings component

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "@narative/gatsby-theme-novela/src/components/Layout";
 import Section from "@narative/gatsby-theme-novela/src/components/Section";
 import SEO from "@narative/gatsby-theme-novela/src/components/SEO";
-import Heading from "@narative/gatsby-theme-novela/src/components/Heading";
+import Headings from "@narative/gatsby-theme-novela/src/components/Headings";
 
 function NotFoundPage() {
   return (
@@ -11,7 +11,7 @@ function NotFoundPage() {
       <SEO />
       <Section>
         <div style={{ marginTop: "100px" }}>
-          <Heading.h1>404: Page Not Found</Heading.h1>
+          <Headings.h1>404: Page Not Found</Heading.h1>
         </div>
       </Section>
     </Layout>
