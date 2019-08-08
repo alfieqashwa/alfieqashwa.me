8d15222437446eb3c24ee9d09751b06c36fff5a5
João Alberto <joaoalberto.contato@gmail.com>
2019-08-07 23:10:47 -0300

    Fix 'Headings' component closing tag

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,7 +11,7 @@ function NotFoundPage() {
       <SEO />
       <Section>
         <div style={{ marginTop: "100px" }}>
-          <Headings.h1>404: Page Not Found</Heading.h1>
+          <Headings.h1>404: Page Not Found</Headings.h1>
         </div>
       </Section>
     </Layout>
