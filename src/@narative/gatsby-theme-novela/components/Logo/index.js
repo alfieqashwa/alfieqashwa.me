f6f45aea5a63d0bfa7521a997e7386ba5fdddb57
alfieqashwa <alfieqashwa@gmail.com>
2019-09-01 16:53:23 +0700

    [fix] -- modifying Logo with basic png. no styling yet

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import celloworld from './celloworld.png';
+
+// TODOS: LOGO NAME ANIMATION...
+export default function Logo() {
+  return (
+    <img
+      width="50"
+      height="50"
+      fill={'primary'}
+      src={celloworld}
+      alt="celloworld"
+    />
+  );
+}
