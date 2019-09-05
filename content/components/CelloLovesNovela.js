7397d086feb7c26ae39e00bf14b0ea74501999e9
alfieqashwa <alfieqashwa@gmail.com>
2019-09-05 16:52:33 +0700

    [styling] -- convert CelloLovesNarative into hooks and jsx pragma

--- content/components/CelloLovesNovela.js
+++ content/components/CelloLovesNovela.js
@@ -1,78 +1,74 @@
-import React, { Component } from 'react';
-import { css } from '@emotion/core';
+/** @jsx jsx */
+import React from 'react';
+import { jsx, css } from 'theme-ui';
 import Fade from 'react-reveal/Fade';
 
-class CelloLovesGatsbyThemeNovela extends Component {
-  constructor(props) {
-    super(props);
+function CelloLovesGatsbyThemeNovela() {
+  const [show, setShow] = React.useState(false);
 
-    this.state = { show: false };
-    this.handleClick = this.handleClick.bind(this);
-  }
+  const handleClick = () => {
+    setShow(s => (s = !s));
+  };
 
-  handleClick() {
-    this.setState({ show: !this.state.show });
-  }
-
-  render() {
-    const { show } = this.state;
-
-    return (
-      <div style={{ textAlign: 'center' }}>
-        <h1 style={{ color: 'lightGreen', fontSize: '28px' }}>
-          <Fade left opposite cascade collapse when={show}>
-            Cello
-            <strong style={{ color: 'pink', fontSize: '42px' }}>
-              <span role="img" aria-labelledby="heart">
-                ❤️
-              </span>
-            </strong>
-            Gatsby Theme Novela
-          </Fade>
-        </h1>
-        <button
-          css={css`
-            color: hotpink;
-            background: papayawhip;
-            font-size: 16px;
-            padding: 5px;
-            border: 1px;
-            border-radius: 2px;
-          `}
-          type="button"
-          onClick={this.handleClick}
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1 sx={{ color: 'accent', fontSize: '28px' }}>
+        <Fade left opposite cascade collapse when={show}>
+          Cello
+          <strong style={{ color: 'red', fontSize: '42px' }}>
+            <span role="img" aria-labelledby="heart">
+              ❤️
+            </span>
+          </strong>
+          Gatsby Theme Novela
+        </Fade>
+      </h1>
+      <Button handleClick={handleClick}>
+        {show ? 'Thank You Again' : 'Thank You'}
+      </Button>
+      <br />
+      <h2 sx={{ color: 'accent', marginTop: '1rem' }}>
+        Dennis Brotzky, Thiago Costa, Brad Tiller, Mack Mansouri from{' '}
+        <a
+          sx={{ color: 'primary' }}
+          href="https://www.narative.co/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          {this.state.show ? 'Thank You Again' : 'Thank You'}
-        </button>
-        <br />
-        <h2 style={{ color: '#AA771C', marginTop: '1rem' }}>
-          Dennis Brotzky, Thiago Costa, Brad Tiller, Mack Mansouri from{' '}
-          <a
-            css={css`
-              color: hotpink;
-            `}
-            href="https://www.narative.co/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Narative.co
-          </a>
-        </h2>
-        <h1 style={{ color: '#e9c16c', marginTop: '1rem' }}>
-          Thank You{' '}
-          <a
-            css={css`
-              color: #663399;
-            `}
-            href="https://www.gatsbyjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GatsbyJS
-          </a>
-        </h1>
-      </div>
-    );
-  }
+          Narative.co
+        </a>
+      </h2>
+      <h1 sx={{ color: 'accent', marginTop: '1rem' }}>
+        Thank You{' '}
+        <a
+          css={css`
+            color: #663399;
+          `}
+          href="https://www.gatsbyjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GatsbyJS
+        </a>
+      </h1>
+    </div>
+  );
 }
+
+const Button = ({ children, handleClick }) => (
+  <button
+    sx={{
+      color: 'hotpink',
+      background: 'papayawhip',
+      fontSize: '18px',
+      padding: '5px',
+      border: '1px',
+      borderRadius: '2px'
+    }}
+    type="button"
+    onClick={handleClick}
+  >
+    {children}
+  </button>
+);
 export default CelloLovesGatsbyThemeNovela;
