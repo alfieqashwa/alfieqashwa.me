d00d1af7e5b75a262cb0ba6b618bbce495412256
alfieqashwa <alfieqashwa@gmail.com>
2019-09-11 16:52:10 +0700

    Created: 1. New Article Props 2. CodeSandbox Component

---
+++ content/components/PropsCodeSandbox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default () => (
+  <iframe
+    src="https://codesandbox.io/embed/objective-kowalevski-465zu?fontsize=14"
+    title="objective-kowalevski-465zu"
+    allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+    style={{
+      width: '100%',
+      height: '500px',
+      border: 0,
+      borderRadius: '4px',
+      overflow: 'hidden'
+    }}
+    sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+  >
+    See the Pen{' '}
+    <a href="https://codesandbox.io/s/objective-kowalevski-465zu?fontsize=14">
+      React In One File
+    </a>
+  </iframe>
+);
