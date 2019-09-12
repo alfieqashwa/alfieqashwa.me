c0cd8f223bd260bd9a8295f0b56d0192d59f46e6
alfieqashwa <alfieqashwa@gmail.com>
2019-09-12 17:45:32 +0700

    [content] --continued article about State, not finished yet

---
+++ content/components/ArticleState/CounterOnlyView.js
@@ -0,0 +1,66 @@
+/** @jsx jsx */
+import { Component } from 'react';
+import { jsx } from 'theme-ui';
+
+function Button(props) {
+  return (
+    <button
+      sx={{
+        color: 'hotpink',
+        background: 'papayawhip',
+        fontSize: '28px',
+        padding: '2px 12px',
+        border: '1px 9px',
+        borderRadius: '4px',
+        ':disabled': {
+          color: 'primary',
+          background: 'grey',
+          opacity: '0.65',
+          cursor: 'not-allowed'
+        }
+      }}
+      type="button"
+      disabled={props.handleDisabled}
+      onClick={props.handleClick}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+class CounterOnlyView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  render() {
+    return (
+      <div sx={{ textAlign: 'center' }}>
+        <h1
+          sx={{
+            color: 'hotpink',
+            fontSize: '48px'
+          }}
+        >
+          Counter
+        </h1>
+        <h2
+          sx={{
+            color: 'hotpink',
+            fontSize: '64px'
+          }}
+        >
+          {this.state.count}
+        </h2>
+        <Button>-</Button>
+        {'  '}
+        <Button>Reset</Button>
+        {'  '}
+        <Button>+</Button>
+      </div>
+    );
+  }
+}
+
+export default CounterOnlyView;
--- content/components/ArticleState/index.js
+++ content/components/ArticleState/index.js
@@ -1,9 +1,11 @@
 import ToggleShowHide from './ToggleShowHide';
 import Clock from './Clock';
 import Counter from './Counter';
+import CounterOnlyView from './CounterOnlyView';
 
 export default {
   ToggleShowHide,
   Clock,
-  Counter
+  Counter,
+  CounterOnlyView
 };
